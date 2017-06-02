2a825462c4640c707f7e89930c473b6b050ca6c3
gacoryy <gacory1234@gmail.com>
2017-06-02 14:11:08 -0300

    Primeiro arquivo

---
+++ js/myplaylist.js
@@ -0,0 +1,52 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01 louca de saudade.mp3',
+        title:'Louca de saudades',
+        artist:'Pearl Jam',
+        rating:4,
+        buy:'http://mundodemusicas.com/lojas-online/',
+        price:'1.99',
+        duration:'3:00',
+        cover:'mix/01.jpg'
+    },
+
+    {
+        mp3:'mix/02 medida certa.mp3',
+        title:'Medida certa',
+        artist:'VHS',
+        rating:4,
+        buy:'http://mundodemusicas.com/lojas-online/',
+        price:'0.99',
+        duration:'3:30',
+        cover:'mix/02 b.jpg'
+    },
+    {
+        mp3:'mix/03 paredes.mp3',
+        title:'Paredes',
+        artist:'unknown',
+        rating:4,
+        buy:'http://mundodemusicas.com/lojas-online/',
+        price:'2.99',
+        duration:'3:10',
+        cover:'mix/03.jpg'
+    },
+    {
+        mp3:'mix/04 vou voando.mp3',
+        title:'Vou voando',
+        artist:'unknown',
+        rating:4,
+        buy:'http://mundodemusicas.com/lojas-online/',
+        price:'0.99',
+        duration:'3:10',
+        cover:'mix/04.jpg'
+    }
+];
